adbc253d4d10e5279fa8110aa98ab969b1302f92
halwave <halenw24@gmail.com>
2025-08-03 15:57:56 -0400

    initial commit, using payload pugin template

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'media-cleaner-plugin/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'media-cleaner-plugin/rsc'
+
+export const importMap = {
+  'media-cleaner-plugin/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'media-cleaner-plugin/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
