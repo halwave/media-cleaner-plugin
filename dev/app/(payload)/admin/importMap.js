7fb07e4585b7c6742f1c9d01a2ba5d192ce9eeed
halwave <halenw24@gmail.com>
2025-08-08 21:20:47 -0400

    updated project name

--- dev/app/(payload)/admin/importMap.js
+++ dev/app/(payload)/admin/importMap.js
@@ -1,9 +1,7 @@
-import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'media-cleaner-plugin/client'
-import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'media-cleaner-plugin/rsc'
+import { BeforeDashboardClient as BeforeDashboardClient_0430b56c8138fea6a264c12df2994c01 } from 'media-cleaner-plugin/client'
+import { BeforeDashboardServer as BeforeDashboardServer_2c9bf621f73ec7338a94b38d763bdde6 } from 'media-cleaner-plugin/rsc'
 
 export const importMap = {
-  'media-cleaner-plugin/client#BeforeDashboardClient':
-    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
-  'media-cleaner-plugin/rsc#BeforeDashboardServer':
-    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+  "media-cleaner-plugin/client#BeforeDashboardClient": BeforeDashboardClient_0430b56c8138fea6a264c12df2994c01,
+  "media-cleaner-plugin/rsc#BeforeDashboardServer": BeforeDashboardServer_2c9bf621f73ec7338a94b38d763bdde6
 }
